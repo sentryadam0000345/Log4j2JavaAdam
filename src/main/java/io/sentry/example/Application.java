cf2804f07ac1b730377cacbfd5c9cfbf6b63b850
adam000034 <aatothfejel@dons.usfca.edu>
2020-07-14 23:38:58 -0700

    1st Commit
    1st Commit

---
+++ src/main/java/io/sentry/example/Application.java
@@ -0,0 +1,300 @@
+package io.sentry.example;
+
+import io.sentry.Sentry;
+import io.sentry.event.Breadcrumb.Level;
+import io.sentry.event.Breadcrumb.Type;
+import io.sentry.event.BreadcrumbBuilder;
+import io.sentry.event.Event;
+import io.sentry.event.EventBuilder;
+import io.sentry.event.UserBuilder;
+import io.sentry.event.helper.ShouldSendEventCallback;
+
+import io.sentry.event.interfaces.ExceptionInterface;
+import io.sentry.event.interfaces.MessageInterface;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+@Controller
+@CrossOrigin
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger = LogManager.getLogger("example.Application");
+
+    private static Map<String, Integer> inventory = new HashMap<>();
+
+    private void checkout(List<Item> cart) {
+        Map<String, Integer> tempInventory = inventory;
+
+        for (Item item : cart) {
+            int currentInventory = tempInventory.get(item.getId());
+            if (currentInventory <= 0) {
+                throw new RuntimeException("No inventory for " + item.getId());
+            }
+
+            tempInventory.put(item.getId(), currentInventory-1);
+        }
+        inventory = tempInventory;
+    }
+
+    @PostMapping(value="/checkout", consumes = "application/json")
+    @ResponseBody
+    public ResponseEntity checkoutCart(@RequestHeader(name = "X-Session-ID", required = true) String sessionId,
+                                       @RequestHeader(name = "X-Transaction-ID", required = true) String transactionId,
+                                       @RequestBody Order order) {
+        try {
+            // set session and transaction id as tags
+            ThreadContext.put("session_id", sessionId);
+            ThreadContext.put("transaction_id", transactionId);
+
+            String userEmail = order.getEmail();
+            logger.info("Processing order for: " + userEmail);
+            
+            // Set the user in the current context.
+            Sentry.getContext().setUser(
+                new UserBuilder().setEmail(userEmail).build()
+            );
+            
+            Sentry.getContext().recordBreadcrumb(
+                new BreadcrumbBuilder().setMessage("Processing checkout for user: " + userEmail).setType(Type.USER).setLevel(Level.INFO).setCategory("custom").build()
+            );
+            
+            // perform checkout
+            checkout(order.getCart());
+            
+        } catch (Exception e) {
+        	
+            // log error + return 500, if exception
+            logger.error("Error while checking out", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Checkout error");
+        }
+
+        // return 200 if checkout was successful
+        return ResponseEntity.status(HttpStatus.OK).body("Success");
+    }
+
+    @RequestMapping("/capture-message")
+    @ResponseBody
+    String captureMessage() {
+    	// MDC extras (added to Sentry event as ADDITIONAL DATA) 
+        ThreadContext.put("extra_key", "extra_value");
+   
+        // NDC extras are sent under 'log4j2-NDC'
+        ThreadContext.push("Extra_details");
+
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warn message"); // warning message that will be sent to Sentry
+        return "Success";
+    }
+
+    @RequestMapping("/handled")
+    @ResponseBody
+    String handledError() {
+        String someLocalVariable = "stack locals";
+        
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            // caught exception that will be sent to Sentry
+            logger.error("Caught exception!", e);
+        }
+        return "Success";
+    }
+    
+    @RequestMapping("/filtered")
+    @ResponseBody
+    String handledFilteredError() {
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            logger.error("Message contains foo!", e);
+        }
+        return "Success";
+    }  
+    
+
+    @RequestMapping("/unhandled")
+    @ResponseBody
+    String unhandledError() {
+    	 String someLocalVariable = "stack locals";
+    	 
+        throw new RuntimeException("Unhandled exception!");
+    }
+
+
+    @RequestMapping("/errorgeneratorone")
+    @ResponseBody
+    String errorGeneratorone() {
+
+        // create an array of Strings
+
+        //int ArrayOfHashCodes[] = new int[] {1186874997, 1186874998,1186874996};
+        int ArrayOfHashCodes[] = new int[] {1186874997};
+
+        int index = (int)(2.0 * Math.random());
+
+        String hashcode = Integer.toString(ArrayOfHashCodes[0]);
+
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            // Add extra data to future events in this context.
+            Sentry.getContext().addExtra("Hashcode", hashcode);
+            logger.error("This error message", e);
+        }
+        return "Success";
+
+    }
+
+    @RequestMapping("/errorgeneratorthree")
+    @ResponseBody
+    String errorGeneratorthree() {
+
+        // create an array of Strings
+        //int ArrayOfHashCodes[] = new int[] {1186874997, 1186874998,1186874996};
+        int ArrayOfHashCodes[] = new int[] {1186874997};
+
+        int index = (int)(2.0 * Math.random());
+
+        String hashcode = Integer.toString(ArrayOfHashCodes[0]);
+
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            // Add extra data to future events in this context.
+            Sentry.getContext().addExtra("Hashcode", hashcode);
+            logger.error("This error message", e);
+        }
+        return "Success";
+
+    }
+
+    @RequestMapping("/errorgeneratorfour")
+    @ResponseBody
+    String errorGeneratorfour() {
+
+        // create an array of Strings
+        int ArrayOfHashCodes[] = new int[] {1186874997, 1186874998,1186874996};
+
+        int index = (int)(2.0 * Math.random());
+
+        String hashcode = Integer.toString(ArrayOfHashCodes[1]);
+
+
+
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            EventBuilder eventBuilder = new EventBuilder()
+                    .withSentryInterface(new MessageInterface("{} failed to provide an email address", "User43"))
+                    //.withMessage("<cite>{} failed to provide an email address</cite>", "User1")
+                    .withLevel(Event.Level.ERROR)
+                    .withSentryInterface(new ExceptionInterface(e));
+
+            // Note that the *unbuilt* EventBuilder instance is passed in so that
+            // EventBuilderHelpers are run to add extra information to your event.
+            Sentry.capture(eventBuilder);
+            //logger.error(hashcode, e);
+        }
+        return "Success";
+
+    }
+
+    @RequestMapping("/errorgeneratortwo")
+    @ResponseBody
+    String errorGeneratortwo() {
+
+        // create an array of Strings
+        int ArrayOfHashCodes[] = new int[] {1186874997, 1186874998,1186874996};
+
+        int index = (int)(2.0 * Math.random());
+
+        String hashcode = Integer.toString(ArrayOfHashCodes[2]);
+
+
+
+        try {
+            int example = 1 / 0;
+        } catch (Exception e) {
+            EventBuilder eventBuilder = new EventBuilder()
+                    .withSentryInterface(new MessageInterface("{} failed to provide an email address", "User1"))
+                    //.withMessage("<cite>{} failed to provide an email address</cite>", "User1")
+                    .withLevel(Event.Level.ERROR)
+                    .withSentryInterface(new ExceptionInterface(e));
+
+            // Note that the *unbuilt* EventBuilder instance is passed in so that
+            // EventBuilderHelpers are run to add extra information to your event.
+            Sentry.capture(eventBuilder);
+            //logger.error(hashcode, e);
+        }
+        return "Success";
+
+    }
+
+    public static void main(String[] args) {
+    	initSentry();
+        inventory.put("wrench", 0);
+        inventory.put("nails", 0);
+        inventory.put("hammer", 1);
+        SpringApplication.run(Application.class, args);
+    }
+
+
+
+
+    private static void initSentry() {
+    	Sentry.init();
+        
+    	Sentry.getStoredClient().setServerName("fe1");
+    	
+    	//Added as tags to Sentry event
+        Sentry.getStoredClient().addTag("dynamicTag1", "1.0");
+
+        Sentry.getStoredClient().addShouldSendEventCallback(new ShouldSendEventCallback() {
+
+            @Override
+            public boolean shouldSend(Event event) {
+
+                System.out.print("EVENT " + event.getExtra().get("Hashcode"));
+                System.out.print("EVENT " + event.getContexts());
+                System.out.print("EVENT " + event.getMessage());
+                System.out.print("GETTTERZ ");
+
+
+                List<String> list = new ArrayList<String>();
+                list.add(event.getMessage());
+                //list.add(Integer.toString((int) event.getExtra().get("Hashcode")));
+                if (event.getExtra().containsKey("Hashcode")) {
+                    list.add(event.getExtra().get("Hashcode").toString());
+                    System.out.print("LIST" +  list);
+                    event.setFingerprint(list);
+                }
+
+                return true;
+            }
+        });
+
+
+
+        
+
+    }
+}
